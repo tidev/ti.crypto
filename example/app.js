c6d2e74ef28c4647e477cfc090080f8944765c24
Dawson Toth <dtoth@appcelerator.com>
2011-11-23 12:19:26 -0700

    [MOD-281] Reworked all examples and docs
    Ti.* pattern is no longer used in the examples or suggested in the documentation. Went through every module. We now use the var Module = require('ti.module'); pattern, very precisely: a) upper case module variable name, and b) single quotes.

--- example/app.js
+++ example/app.js
@@ -1,25 +1,25 @@
 var App = {
-	controllers: {},
-	crypto: null,
-	
-	loadObject: function(type, name, params) {
-		if (App[type] == null) {
-			Ti.API.warn('Trying to load an object that does not exist in the App namespace');
-			return false;
-		} else if (App[type][name] == null) {
-			Ti.include(type.toLowerCase() + '/' + name + '.js');
-			Ti.API.info(type + ' ' + name + ' loaded');
-			return new App[type][name](params);
-		} else {
-			Ti.API.info(type + ' ' + name + ' already loaded');
-			return new App[type][name](params);
-		}
-	}
+    controllers: {},
+    crypto: null,
+
+    loadObject: function (type, name, params) {
+        if (App[type] == null) {
+            Ti.API.warn('Trying to load an object that does not exist in the App namespace');
+            return false;
+        } else if (App[type][name] == null) {
+            Ti.include(type.toLowerCase() + '/' + name + '.js');
+            Ti.API.info(type + ' ' + name + ' loaded');
+            return new App[type][name](params);
+        } else {
+            Ti.API.info(type + ' ' + name + ' already loaded');
+            return new App[type][name](params);
+        }
+    }
 };
 
 // Load the crypto modules
-App.crypto = require('ti.crypto');
+var Crypto = require('ti.crypto');
 
-Ti.include ( 'ui.js' );
+Ti.include('ui.js');
 
 App.UI.createAppWindow().open();
