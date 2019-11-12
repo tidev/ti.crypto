86ef5fccfbf3a8877f13a9ba5f1e4e8a75f99859
Chris Williams <chris.a.williams@gmail.com>
2019-11-12 15:39:07 -0500

    fix(android): override apiname on module

--- android/src/ti/crypto/CryptoModule.java
+++ android/src/ti/crypto/CryptoModule.java
@@ -174,4 +174,8 @@ public class CryptoModule extends KrollModule {
   @Kroll.constant public static final String TYPE_BLOB = "blob";
   @Kroll.constant public static final String TYPE_HEXSTRING = "hexstring";
   @Kroll.constant public static final String TYPE_BASE64STRING = "base64string";
+
+  public String getApiName() {
+ 	return "Ti.Crypto";
+  }
 }
