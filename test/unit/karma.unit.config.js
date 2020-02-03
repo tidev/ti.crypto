eb09c1693b56ede52ec70ae19e412a1e3c2946c6
Chris Williams <chris.a.williams@gmail.com>
2020-02-03 13:11:37 -0500

    build: use sdk 9.0.0.v20200130113429

--- test/unit/karma.unit.config.js
+++ test/unit/karma.unit.config.js
@@ -12,7 +12,7 @@ module.exports = config => {
 			'karma-*'
 		],
 		titanium: {
-			sdkVersion: config.sdkVersion || '8.2.1.GA'
+			sdkVersion: config.sdkVersion || '9.0.0.v20200130113429'
 		},
 		customLaunchers: {
 			android: {
